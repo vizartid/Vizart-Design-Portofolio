3c4bb66ceeb8d0be49f0ede48569f5494fb8c227
Muhammad Yusuf Aditiya <muhammad.125490072@student.itera.ac.id>
2025-08-16 21:16:34 +0700

    Updated branding.ts

--- client/src/data/branding.ts
+++ client/src/data/branding.ts
@@ -1,6 +1,6 @@
 export const branding = {
   logoUrl: "/attached_assets/Profile-foto.png",
   logoText: "Vizart",
-  footerLogoUrl: "attached_assets/logo-vizart.png",
+  footerLogoUrl: "/attached_assets/logo-vizart.png",
   footerLogoText: "Vizart"
 };
