51d063162af166cc89baa205124807c65562481e
Yusuf <vizart.id@gmail.com>
2025-08-19 09:49:06 +0700

    footer

--- client/src/data/branding.ts
+++ client/src/data/branding.ts
@@ -1,6 +1,6 @@
 export const branding = {
   logoUrl: "attached_assets/logo-vizart.png",
   logoText: "Vizart",
-  footerLogoUrl: "/home/project/attached_assets/profile-foto.jpg",
+  footerLogoUrl: "attached_assets/logo-vizart.png",
   footerLogoText: "Vizart"
 };
