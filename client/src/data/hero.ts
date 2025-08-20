6e1056bd92441a39f028f49cdc9f08d89a0cb738
Muhammad Yusuf Aditiya <muhammad.125490072@student.itera.ac.id>
2025-08-20 04:32:06 +0000

    Update: Delete img folder and Remove icon

--- client/src/data/hero.ts
+++ client/src/data/hero.ts
@@ -14,35 +14,12 @@ export const hero = {
       logoUrl:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
     },
-    {
-      name: "Adobe Photoshop",
-      color: "blue-500",
-      icon: "Image",
-      logoUrl: "https://skillicons.dev/icons?i=ps",
-    },
-    {
-      name: "Adobe XD",
-      color: "pink-500",
-      icon: "Layers",
-      logoUrl: "https://skillicons.dev/icons?i=xd",
-    },
+
     {
       name: "Adobe Illustrator",
       color: "yellow-500",
       icon: "PenTool",
       logoUrl: "https://skillicons.dev/icons?i=ai",
-    },
-    {
-      name: "Webflow",
-      color: "blue-600",
-      icon: "Globe",
-      logoUrl: "https://cdn.simpleicons.org/webflow/4353FF",
-    },
-    {
-      name: "JavaScript",
-      color: "yellow-400",
-      icon: "Code",
-      logoUrl: "https://cdn.simpleicons.org/javascript/F7DF1E",
     },
     {
       name: "React",
