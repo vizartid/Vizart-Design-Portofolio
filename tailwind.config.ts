fed554aa4e1f3eac10f367d20ac3f5c45eafb31f
Muhammad Yusuf Aditiya <muhammad.125490072@student.itera.ac.id>
2025-08-24 02:56:13 +0000

    Restored to '1eb3b568aa5a867cc8422a3d4e4b3abefe203af5'
    Replit-Restored-To: 1eb3b568aa5a867cc8422a3d4e4b3abefe203af5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ export default {
   content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        'mobile': '0px',
+        'tablet': '768px',
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
